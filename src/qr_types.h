3215139ef8a3a91f59d84a1780fba7e917abe07d
Kilian Chung <kilian.chung@web.de>
2025-11-08 16:01:22 +0100

    important update

--- src/qr_types.h
+++ src/qr_types.h
@@ -4,7 +4,8 @@
 #include <stddef.h>
 
 // Error correction levels
-typedef enum {
+typedef enum
+{
     QR_EC_LEVEL_L = 0,
     QR_EC_LEVEL_M,
     QR_EC_LEVEL_Q,
@@ -15,15 +16,20 @@ typedef enum {
 #define QR_VERSION_COUNT 40
 
 // Data encoding modes
-typedef enum {
+typedef enum
+{
     QR_MODE_BYTE
 } qr_encoding_mode;
 
-typedef struct {
+typedef struct
+{
     qr_ec_level ec_level;
     qr_encoding_mode encoding_mode;
     unsigned version;
-    size_t capacity;
+    size_t mask;
+
+    int *data;  // TODO: rename to matrix
+    size_t side_length;
 } qr_code;
 
 #endif // QR_TYPES_H
