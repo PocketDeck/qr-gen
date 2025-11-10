439df1c0845d9809106a4db82a58c0ba7d950e42
Kilian Chung <kilian.chung@web.de>
2025-11-10 21:49:19 +0100

    finish?

--- qr/ecc.h
+++ qr/ecc.h
@@ -3,15 +3,7 @@
 
 #include <qr/types.h>
 
-typedef struct
-{
-    uint8_t *data;
-    uint8_t *ecc;
-    size_t data_length;
-    size_t ecc_length;
-    uint8_t *generator;
-} qr_ec;
-
 void qr_ec_encode(qr_code *qr);
+void qr_interleave_codewords(qr_code *qr);
 
 #endif // QR_ECC_H
