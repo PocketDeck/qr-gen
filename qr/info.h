43b946007597f5086bfa88318a1ce394a7923638
Kilian Chung <kilian.chung@web.de>
2026-06-02 19:03:50 +0200

    typo guard

--- qr/info.h
+++ qr/info.h
@@ -1,9 +1,9 @@
-#ifndef QR_INFO_h
-#define QR_INFO_h
+#ifndef QR_INFO_H
+#define QR_INFO_H
 
 #include <qr/types.h>
 
 void qr_format_info_apply(qr_code *qr);
 void qr_version_info_apply(qr_code *qr);
 
-#endif // QR_INFO_h
+#endif // QR_INFO_H
