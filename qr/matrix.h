8d31635ce560985231066c9dc8c51298f9d9dfb1
Kilian Chung <kilian.chung@web.de>
2026-06-03 14:18:46 +0200

    remove include

--- qr/matrix.h
+++ qr/matrix.h
@@ -3,7 +3,6 @@
 
 #include <qr/types.h>
 #include <stddef.h>
-#include <stdio.h>
 
 qr_module qr_matrix_get(const qr_code *qr, size_t i, size_t j);
 void qr_matrix_set(qr_code *qr, size_t i, size_t j, qr_module value);
